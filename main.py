d94406a6fffad9d8c8b197301008764bdcf56769
Chenyang <190864387@qq.com>
2023-04-23 11:48:22 +0800

    引入爬虫API，更新文档

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
+import os
 from database import DataBase
 window = Tk()
 window.title("First Window")
 window.mainloop()
-
+a=os.system("ping 192.168.1.101")
 new = DataBase()
