681c71445d2d6e5b36e26bd72afd76ebb6635187
Chenyang <190864387@qq.com>
2023-04-23 15:28:25 +0800

    加入并测试了数据库的增加和修改功能

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
-import sqlite3,os
-
+import sqlite3,os,copy
+MY_SUCCESS = 1
+MY_ERROR_NO_PAPERNO = 0
 
 
 # 定义本地文献数据库类
 class DataBase:
     # 定义表格属性
-    FIELD_LIST = ["Num", "PaperTitle", "Author", "YearofPublish", "Conference", "Tags",
+    FIELD_LIST = ["No", "ReadOrNot", "YearofPublish", "PaperTitle", "Author",  "Conference", "Tags",
                    "LastReadDate", "URL", "Q1", "Q2", "Q3", "Q4", "Q5",
                   ]
     # 定义数据库位置（相对）
@@ -20,10 +21,11 @@ class DataBase:
         else:
             self.con = sqlite3.connect(DataBase.DB_REL_PATH)
             init_text = '''CREATE TABLE PAPERS(
-            Num INT PRIMARY KEY AUTOINCREMENT,
+            No INTEGER PRIMARY KEY AUTOINCREMENT,
+            ReadOrNot INT,
+            YearofPublish INT,
             PaperTitle TEXT,
             Author TEXT,
-            YearofPublish INT,
             Conference TEXT,
             Tags TEXT,
             LastReadDate TEXT,
@@ -32,37 +34,107 @@ class DataBase:
             Q2  TEXT,
             Q3  TEXT,
             Q4  TEXT,
-            Q5  TEXT,
+            Q5  TEXT
             );'''
             self.con.execute(init_text)
             self.con.commit()
         
-        self.con.close()
+       # self.con.close()
         return None
-# TODO:接受字典，插入数据
-def add_data(self, info:dict):
-    pass
     
-# TODO:接受字典，更改数据
-def modi_data(self, info:dict):
-    pass
 
-#TODO:接受字典，删除数据
-def modi_data(self, info:dict):
-    pass
+# TODO:接受字典，插入数据
+    def add_data(self, info:dict):
+        fields = "ReadOrNot"  # 字段
+        values = "0"  # 值
 
-#TODO : 或根据主键删除
-def modi_data_num(self, num:int):
-    pass
+        add_fields = copy.deepcopy(DataBase.FIELD_LIST)
+        add_fields.remove("No")
+        add_fields.remove("ReadOrNot")
+        add_fields.remove("YearofPublish")
 
-# TODO:展示所有paper
-def show_all_paper(self):
-    pass
+        if info.get("YearofPublish"):
+            fields += "," + "YearofPublish"
+            values += "," + str(info.get("YearofPublish"))
+        for item in add_fields:
+            if info.get(item):
+                fields += "," + item
+                values += ",\"" + str(info.get(item)) + "\""
+            else:
+                fields += "," + item
+                values += ",\"\""
 
-#TODO: 查找某个paper
-def search_paper(self, info:dict):
-    pass
+        self.con.execute(f'''
+            insert into PAPERS ({fields}) values ({values});     
+            ''')
+        self.con.commit()
+        
+        return MY_SUCCESS
+        
+    # TODO:接受字典，更改数据
+    def modi_data(self, info:dict):
+        update = ""
 
-#TODO: 批量加入大量paper，调用爬虫API
-def batch_add_data(self):
-    pass
+        no = info.get("No")
+        
+        if not no:
+            return MY_ERROR_NO_PAPERNO
+            
+        
+        modi_fields = copy.deepcopy(DataBase.FIELD_LIST)
+        modi_fields.remove("No")
+        modi_fields.remove("ReadOrNot")
+        modi_fields.remove("YearofPublish")
+        print(update,no)
+        # 依次读取待修改的属性
+        if info.get("ReadOrNot"):
+            update += "ReadOrNot=" + str(info.get("ReadOrNot")) + ","
+        if info.get("YearofPublish"):
+            update += "YearofPublish=" + str(info.get("YearofPublish")) + ","
+        for item in modi_fields:
+            if info.get(item):
+                update += item + "=\"" + info.get(item) + "\","
+        # 删除最后多余的 ,
+        try:
+            if update[-1] == ",":
+                update = update[:-1]
+        except:
+            pass
+            
+        print(update,no)
+        self.con.execute(f'''
+            UPDATE PAPERS
+            SET {update}
+            WHERE No = {no};
+        ''')
+        self.con.commit()
+
+        return MY_SUCCESS
+
+    #TODO:接受字典，删除数据
+    def del_data(self, info:dict):
+        pass
+
+    #TODO : 或根据主键删除
+    def modi_data_no(self, no:int):
+        pass
+
+    # TODO:展示所有paper
+    def show_all_paper(self):
+        ret_list = list(self.con.execute('''
+                select * from PAPERS
+                ORDER BY YearofPublish, PaperTitle  DESC;
+                '''))
+ 
+        print("下面是所有论文")
+        print(ret_list)
+        return ret_list
+    
+
+    #TODO: 查找某个paper
+    def search_paper(self, info:dict):
+        pass
+
+    #TODO: 批量加入大量paper，调用爬虫API
+    def batch_add_data(self):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from tkinter import *
 import os
 from database import DataBase
-window = Tk()
-window.title("First Window")
-window.mainloop()
-a=os.system("ping 192.168.1.101")
+#window = Tk()
+#window.title("First Window")
+#window.mainloop()
+#a=os.system("ping 192.168.1.101")
 new = DataBase()
+info = {'No' : 2,'ReadOrNot' : 1,'YearofPublish': 2019,'PaperTitle' :'find bug in real world','Author' :'dod',
+            'Conference': 'CCS',
+            'Tags' :'fucker',
+            'LastReadDate': '20-9',
+            'URL' :'why',
+            'Q1'  :'why',
+            'Q2'  :'why',
+            'Q3'  :'why',
+            'Q4'  :'why',
+            'Q5'  :'why'}
+#new.add_data(info)
+info2 = {'No' : 2,'ReadOrNot' : 1,'YearofPublish': 2023,'PaperTitle' :'software develo','Author' :'alice',
+            'Conference': 'usenix',
+            'Tags' :'fucker',
+            'LastReadDate': '20-9',
+            'URL' :'why',
+            'Q1'  :'why',
+            'Q2'  :'why',
+            'Q3'  :'why',
+            'Q4'  :'why',
+            'Q5'  :'why'}
+new.modi_data(info2)
+new.show_all_paper()
+
